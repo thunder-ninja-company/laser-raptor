c16dfe866a7b01acbd9b3f2da04b49498fb42f14
Daniel Donahoe <daniel.donahoe@gmail.com>
2022-05-13 19:00:43 -0700

    feat(amplify): neato!

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome DEVLOP BRANCHO  <a href="https://nextjs.org">Next.js!</a>
+          Welcome 123123 BRANCHO  <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
