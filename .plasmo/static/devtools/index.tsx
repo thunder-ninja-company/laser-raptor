b94b682e8823d3500f154533a03c5ae5a768955f
Dan Donahoe <daniel.donahoe@gmail.com>
2022-06-08 13:32:02 -0400

    feat(plasmo): im confused

---
+++ .plasmo/static/devtools/index.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import * as Component from "~devtools"
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+root.render(<Component.default />)
