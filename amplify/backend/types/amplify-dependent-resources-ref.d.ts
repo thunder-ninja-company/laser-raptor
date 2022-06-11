8097bd7172d5a8ad68ff2b93cbf50efba0a6fefc
Dan Donahoe <daniel.donahoe@gmail.com>
2022-06-10 22:12:18 -0400

    feat(amplify): annoying config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "thundervolcanoe935e9bc": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
