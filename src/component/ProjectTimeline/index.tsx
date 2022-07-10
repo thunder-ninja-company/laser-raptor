a86a6ed38fdb516f4a2afe28866b99eb9eea260a
Dan Donahoe <daniel.donahoe@gmail.com>
2022-07-10 01:06:06 -0400

    feat(dunno): testing

--- src/component/ProjectTimeline/index.tsx
+++ src/component/ProjectTimeline/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, useMantineTheme } from "@mantine/core";
+import { Grid, Paper, useMantineTheme } from "@mantine/core";
 import React from "react";
 
 import type { Props } from "./type";
@@ -17,6 +17,11 @@ const ProjectTimeline: React.FC<Props> = () => {
             <h1>
                 {'Project TImeline v1.o'}
             </h1>
+            <Grid>
+            <Grid.Col span={4}>1</Grid.Col>
+            <Grid.Col span={4}>2</Grid.Col>
+            <Grid.Col span={4}>3</Grid.Col>
+            </Grid>
         </Paper>
     );
 };
