2718642fbe423eec93a9279cff816bd3bd3ffbee
Dan Donahoe <daniel.donahoe@gmail.com>
2022-07-14 14:49:05 -0400

    feat(cloud): fixing up example

---
+++ src/assets/style.tsx
@@ -0,0 +1,11 @@
+import type { CSSObject } from "@mantine/core";
+
+// styles object is compatible with every Mantine component
+export const shareThisStyle: CSSObject = {
+    border: "1px solid #eee",
+    color: "#999",
+
+    "&:hover": {
+        backgroundColor: "#0f3",
+    },
+};
---
+++ src/project/gist/constants.tsx
@@ -0,0 +1,2 @@
+
+export const PROJECT_NAME = 'gist';
