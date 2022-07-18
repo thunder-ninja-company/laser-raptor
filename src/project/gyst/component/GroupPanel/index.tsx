ba3934d302294523ed876739658a35d01906cc9e
Dan Donahoe <daniel.donahoe@gmail.com>
2022-07-18 11:44:21 -0400

    feat(redux): setting up redux shape

--- src/project/gyst/component/GroupPanel/index.tsx
+++ src/project/gyst/component/GroupPanel/index.tsx
@@ -1,12 +1,15 @@
 import { Box } from "@mantine/core";
 import React from "react";
 import GroupPanelItem from "project/gyst/component/GroupPanelItem";
+import { useStyles } from "./style";
 
 import type { Props } from "./type";
 
 export default function GroupPanel({ id, value }: Props) {
+    const style = useStyles();
+
     return (
-        <Box id={id} sx={{ padding: "20px", margin: "20px" }}>
+        <Box id={id} className={style.classes.groupPanel}>
             {`GROUP PANEL ${id} items: `}
             {value.groupPanelItems.map((item, index) => (
                 <GroupPanelItem
--- src/project/gyst/component/GroupPanelItem/style.tsx
+++ src/project/gyst/component/GroupPanelItem/style.tsx
@@ -8,43 +8,4 @@ export const useStyles = createStyles((theme) => ({
         justifyContent: "space-between",
     },
 
-    burger: {
-        [theme.fn.largerThan("sm")]: {
-            display: "none",
-        },
-    },
-
-    links: {
-        paddingTop: theme.spacing.lg,
-        height: 100,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-
-        [theme.fn.smallerThan("sm")]: {
-            display: "none",
-        },
-    },
-
-    mainLinks: {
-        marginRight: -theme.spacing.sm,
-    },
-
-    mainLink: {
-        textTransform: "uppercase",
-        fontSize: 13,
-        color:
-            theme.colorScheme === "dark"
-                ? theme.colors.dark[1]
-                : theme.colors.gray[6],
-        padding: `7px ${theme.spacing.sm}px`,
-        fontWeight: 700,
-        borderBottom: "2px solid transparent",
-        transition: "border-color 100ms ease, color 100ms ease",
-
-        "&:hover": {
-            color: theme.colorScheme === "dark" ? theme.white : theme.black,
-            textDecoration: "none",
-        },
-    },
 }));
