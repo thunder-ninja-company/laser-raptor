42747d3e9bb0eb59cbd1cce06e4c870cd62c202a
Daniel Donahoe <daniel.donahoe@gmail.com>
2022-08-11 15:21:13 -0400

    feat(hooray): test passed, time for a walk

--- src/project/gyst/component/DragGrid/__tests__/lalalalalala.test.tsx
+++ src/project/gyst/component/DragGrid/__tests__/lalalalalala.test.tsx
@@ -2,22 +2,17 @@ import Logic from 'gyst/component/DragGrid/logic'
 import { initialDragGrid } from '../constant'
 
 
-describe('When NOT in Firefox', { browser : '!firefox' }, () => {
-    it('Shows warning', () => {
+describe('Please just make webpack cooperate', () => {
+    it('Uhggggg', () => {
 
+        console.log('Logic')
+        console.log(Logic)
+        // const newDragGrid = Logic.duplicateItem(initialDragGrid, 'fuck')
 
-        const newDragGrid = Logic.duplicateItem(initialDragGrid, 'fuck')
-
-        expect('bbbllllaaaaa').to.equal('bbbllllaaaaa')
+        expect('bbbllllaaaaa').toBe('bbbllllaaaaa')
     })
 
-    it('Links to browser compatibility doc', () => {
-        expect('bbbllllaaaaa').to.equal('bbbllllaaaaa')
-    })
-})
-
-describe('go to the onion in AppBody test directory', () => {
-    it('passes', () => {
-        cy.visit('https://theonion.com')
-    })
+    // it('Links to browser compatibility doc', () => {
+    //     expect('bbbllllaaaaa').to.equal('bbbllllaaaaa')
+    // })
 })
